818e88c7aaa2d358058f102c982bc6b6e99d2b3f
ICDeadPpl <jan@biffstek.se>
2015-10-10 09:44:47 +0200

    Initial commit.

---
+++ mobileread-showfirstunread.user.js
@@ -0,0 +1,38 @@
+// ==UserScript==
+// @name       mobileread.com forum - goto first unread redirect
+// @namespace  http://www.fmbv.nu/
+// @version    0.1
+// @description  mobileread.com forum - goes to first unread post.
+// @match      http*://*mobileread.com/forums*
+// @copyright  2015, Jan Karjalainen
+// @grant       none
+// ==/UserScript==
+
+
+(function() {
+
+    try {
+
+        textLink="View First Unread";
+
+        nPage=-1;
+        
+	// opens 1.st such a link in tab
+
+        for( i=0; i < document.links.length; i++ )
+
+            if( document.links[ i ].innerHTML.match( textLink ))
+
+		window.location.href=document.links[i].href
+    }
+
+    catch (e) {
+
+        GM_log( 'mobileread.com forum - goto first unread Redirect - script exception: ' + e );
+        alert ( 'mobileread.com - goto first unread Redirect - script exception: ' + e );
+
+    }
+
+}
+
+)();
