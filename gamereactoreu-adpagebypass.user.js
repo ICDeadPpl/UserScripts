8ce9bbaa6b2dde5b27f3a8fb7e33604040f93ed3
ICDeadPpl <jan@biffstek.se>
2019-11-15 21:39:38 +0100

    Fixed bug, added exception handling.

--- gamereactoreu-adpagebypass.user.js
+++ gamereactoreu-adpagebypass.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name       Gamereactor.eu - ad page bypass
 // @namespace  http://www.fmbv.nu/
-// @version    0.2
+// @version    0.3
 // @description  Gamereactor.eu - Bypass ad pages.
 // @match      http://www.gamereactor.eu/*
-// @match      https://www.gamereactor.ey/*
+// @match      https://www.gamereactor.eu/*
 // @grant       none
 // @copyright  2013, Jan Karjalainen
 // ==/UserScript==
@@ -17,7 +17,7 @@
         textLink="Continue to";
 
         nPage=-1;
-        
+
 	// opens 1.st such a link in tab
 
         for( i=0; i < document.links.length; i++ )
@@ -25,4 +25,16 @@
             if( document.links[ i ].innerHTML.match( textLink ))
 
 		window.location.href=document.links[i].href
-    
+    }
+
+    catch (e) {
+
+        GM_log( 'Gamereactor.eu - ad page bypasst - script exception: ' + e );
+
+        alert ( 'Gamereactor.eu - ad page bypass - script exception: ' + e );
+
+    }
+
+}
+
+)();
