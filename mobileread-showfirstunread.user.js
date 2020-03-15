7bc80dc995c018cc7dcf573da1d5e20e7ae7b31b
ICDeadPpl <jan@biffstek.se>
2020-03-15 07:51:17 +0100

    Tidying up variable assignments.

--- mobileread-showfirstunread.user.js
+++ mobileread-showfirstunread.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       mobileread.com forum - goto first unread redirect
 // @namespace  http://www.fmbv.nu/
-// @version    0.2
+// @version    0.4
 // @description  mobileread.com forum - goes to first unread post.
 // @match      http://www.mobileread.com/forums*
 // @match      https://www.mobileread.com/forums*
@@ -14,17 +14,19 @@
 
     try {
 
-        textLink="View First Unread";
+        var textLink="View First Unread";
 
-        nPage=-1;
-        
+        var nPage=-1;
+
 	// opens 1.st such a link in tab
 
-        for( i=0; i < document.links.length; i++ )
+        for( var i=0; i < document.links.length; i++ ) {
 
-            if( document.links[ i ].innerHTML.match( textLink ))
+            if( document.links[ i ].innerHTML.match( textLink )) {
 
-		window.location.href=document.links[i].href
+                window.location.href=document.links[i].href
+            }
+        }
     }
 
     catch (e) {
