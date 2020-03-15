94bbed9012b6c876f78a00b606237a6ef38e5dab
ICDeadPpl <jan@biffstek.se>
2020-03-15 07:56:45 +0100

    Create MobilereadPostsInNewTab.user.js

---
+++ MobilereadPostsInNewTab.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name       Mobileread Forum posts in new tab
+// @namespace  https://www.bitshark.se
+// @version    0.3
+// @description  Makes all links to forum posts open in a new tab
+// @description  Edit the "match" address below for your needs.
+// @match      https://www.mobileread.com/forums/subscription.php
+// @grant none
+// @copyright  2020+ Jan Karjalainen
+// ==/UserScript==
+
+var pattern = new RegExp('/showthread');
+var anchors = document.getElementsByTagName('a');
+for (var i = 0; i < anchors.length; i++) {
+    var a = anchors[i];
+    if (pattern.test(a.href)){
+        a.setAttribute("target", "_blank");
+    }
+}
