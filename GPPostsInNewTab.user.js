42a3f3ed20a8f59d44c4d9126f65cfbc15793190
ICDeadPpl <jan.karjalainen@gmail.com>
2024-09-17 08:47:41 +0200

    Update GPPostsInNewTab.user.js

--- GPPostsInNewTab.user.js
+++ GPPostsInNewTab.user.js
@@ -1,22 +1,15 @@
 // ==UserScript==
-// @name       Unraid Forum posts in new tab
+// @name       GP.se posts in new tab
 // @namespace  https://www.bitshark.se
 // @version    0.1
-// @description  Makes all links to forum posts open in a new tab
-// @description  Edit the "match" address below for your needs.
+// @description  Makes all links open in a new tab
 // @match      https://www.gp.se/*
 // @grant none
-// @copyright  2020+ Jan Karjalainen
+// @copyright  2024+ Jan Karjalainen
 // ==/UserScript==
 
-var pattern = new RegExp('/topic/|/prereleases/|stable-releases/');
 var anchors = document.getElementsByTagName('a');
 for (var i = 0; i < anchors.length; i++) {
     var a = anchors[i];
-    if (pattern.test(a.href)){
         a.setAttribute("target", "_blank");
-    } else {
-        a.setAttribute("target", "_blank");
-    }
-  
 }
