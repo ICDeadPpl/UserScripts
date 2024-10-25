6e6419c1d87dfb9f20f87443c8b8f9fb300d0da5
ICDeadPpl <jan.karjalainen@gmail.com>
2024-10-25 08:58:20 +0200

    Update GPPostsInNewTab.user.js
    Added more match URLs

--- GPPostsInNewTab.user.js
+++ GPPostsInNewTab.user.js
@@ -1,9 +1,20 @@
-// ==UserScript==
+/// ==UserScript==
 // @name       GP.se posts in new tab
 // @namespace  https://www.bitshark.se
 // @version    0.1
 // @description  Makes all links open in a new tab
-// @match      https://www.gp.se/*
+// @match      https://www.gp.se/nyheter/*
+// @match      https://www.gp.se/sport/*
+// @match      https://www.gp.se/ekonomi/*
+// @match      https://www.gp.se/ledare/*
+// @match      https://www.gp.se/kultur*/*
+// @match      https://www.gp.se/debatt/*
+// @match      https://www.gp.se/poddar/*
+// @match      https://www.gp.se/politik/*
+// @match      https://www.gp.se/krim/*
+// @match      https://www.gp.se/nyhetsshowen/*
+// @match      https://www.gp.se/livsstil/*
+// @match      https://www.gp.se/fria-ord/*
 // @grant none
 // @copyright  2024+ Jan Karjalainen
 // ==/UserScript==
