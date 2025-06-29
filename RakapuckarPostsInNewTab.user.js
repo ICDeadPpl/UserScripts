d6912bcbed5fff20d62bf1cb381d72771a188e5e
ICDeadPpl <jan.karjalainen@gmail.com>
2025-06-29 07:55:15 +0200

    Update RakapuckarPostsInNewTab.user.js
    Fixed matched urls

--- RakapuckarPostsInNewTab.user.js
+++ RakapuckarPostsInNewTab.user.js
@@ -1,9 +1,15 @@
 // ==UserScript==
 // @name       rakapuckar.com posts in new tab
 // @namespace  https://www.bitshark.se
-// @version    0.1
+// @version    0.2
 // @description  Makes all links open in a new tab
-// @match      https://rakapuckar.com/*
+// @match      https://rakapuckar.com
+// @match      https://rakapuckar.com/smatt-och-gott
+// @match      https://rakapuckar.com/samarbetsartiklar/
+// @match      https://rakapuckar.com/hockey/herr/
+// @match      https://rakapuckar.com/hockey/dam/
+// @match      https://rakapuckar.com/podd/
+// @match      https://rakapuckar.com/arkiv/
 // @grant none
 // @copyright  2024+ Jan Karjalainen
 // ==/UserScript==
